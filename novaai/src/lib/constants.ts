5ad4e36d1c01e42901119aa4fc02bc470d9ba8d0
Sabrihan Akar <kamadaska@gmail.com>
2026-08-18 13:29:21 +0300

    Upload novaai/src/lib/constants.ts via Web Uploader

---
+++ novaai/src/lib/constants.ts
@@ -0,0 +1,8 @@
+export const HERO_VIDEO_URL =
+  'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260729_102822_0e6c87e8-c141-4744-bf32-ad30db296371.mp4';
+
+export const HERO_VIDEO_LOCAL = '/hero.mp4';
+export const HERO_POSTER_LOCAL = '/hero-poster.jpg';
+
+export const MITHA_PORTRAIT_URL =
+  'https://images.higgs.ai/?default=1&output=webp&url=https%3A%2F%2Fd8j0ntlcm91z4.cloudfront.net%2Fuser_38xzZboKViGWJOttwIXH07lWA1P%2Fhf_20260728_050334_5b076e26-0ce7-4898-b432-d764190e448f.png&w=1280&q=85';
